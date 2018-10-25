0a3a96e7dda1d393b4bc6c250f90744e37b55bf1
Moustapha Mbengue <m2consulting.eu@gmail.com>
2018-10-25 16:08:56 +0000

    Ajout du plugin tomcat pour un déploiement en mode Dev

--- common/src/main/java/sn/smart/eco/common/model/LevelTypeId.java
+++ common/src/main/java/sn/smart/eco/common/model/LevelTypeId.java
@@ -1,77 +1,76 @@
 package sn.smart.eco.common.model;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-
 import java.io.Serializable;
 
 public class LevelTypeId implements Serializable {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  private String name;
-  private Integer depth;
-  private PlanType plan;
+	private String name;
+	private Integer depth;
+	private PlanType plan;
 
-  public LevelTypeId() {}
+	public LevelTypeId() {
+	}
 
-  public LevelTypeId(String name, Integer depth, PlanType plan) {
-    super();
-    this.name = name;
-    this.depth = depth;
-    this.plan = plan;
-  }
+	public LevelTypeId(String name, Integer depth, PlanType plan) {
+		super();
+		this.name = name;
+		this.depth = depth;
+		this.plan = plan;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public Integer getDepth() {
-    return depth;
-  }
+	public Integer getDepth() {
+		return depth;
+	}
 
-  public void setDepth(Integer depth) {
-    this.depth = depth;
-  }
+	public void setDepth(Integer depth) {
+		this.depth = depth;
+	}
 
-  public PlanType getPlan() {
-    return plan;
-  }
+	public PlanType getPlan() {
+		return plan;
+	}
 
-  public void setPlan(PlanType plan) {
-    this.plan = plan;
-  }
+	public void setPlan(PlanType plan) {
+		this.plan = plan;
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
 
-    if (!instanceOf(obj.getClass()).equals(getClass())) {
-      return false;
-    }
+		if (!(obj instanceof LevelTypeId)) {
+			return false;
+		}
 
-    LevelTypeId ltype = (LevelTypeId) obj;
-    if (this == ltype) {
-      return true;
-    }
+		LevelTypeId ltype = (LevelTypeId) obj;
+		if (this == ltype) {
+			return true;
+		}
 
-    if (hashCode() == ltype.hashCode()) {
-      return true;
-    }
+		if (hashCode() == ltype.hashCode()) {
+			return true;
+		}
 
-    if (name.equals(ltype.name) && depth.equals(ltype.depth) && plan.equals(ltype.plan)) {
-      return true;
-    }
+		if (name.equals(ltype.name) && depth.equals(ltype.depth) && plan.equals(ltype.plan)) {
+			return true;
+		}
 
-    return false;
-  }
+		return false;
+	}
 
-  @Override
-  public int hashCode() {
-    return name.hashCode() + depth + plan.hashCode();
-  }
+	@Override
+	public int hashCode() {
+		return name.hashCode() + depth + plan.hashCode();
+	}
 }
