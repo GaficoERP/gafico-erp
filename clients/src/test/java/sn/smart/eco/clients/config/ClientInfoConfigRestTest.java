562b697473382edd9c530cf84d861eb81b67d226
m2Consulting <m2consulting.eu@gmail.com>
2018-10-28 13:47:02 +0000

    (chore) 6.2 - Gestion (Crud) Client (Back) - oubli

--- clients/src/test/java/sn/smart/eco/clients/config/ClientInfoConfigRestTest.java
+++ clients/src/test/java/sn/smart/eco/clients/config/ClientInfoConfigRestTest.java
@@ -1,7 +1,5 @@
 package sn.smart.eco.clients.config;
 
-<<<<<<< HEAD
-=======
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @Import(ClientInfoConfigTest.class)
 @EnableWebMvc
->>>>>>> aa5f19c... (chore) 6.2 - Gestion (Crud) Client (Back) - oubli
 public class ClientInfoConfigRestTest {
 
 }
