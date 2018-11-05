671f0175bc62987e6b59e85fd0e8be8043776e5a
Mor NDIAYE <mor.ndiaye@teamxgroup.com>
2018-11-05 16:18:21 +0000

    chore(security): enable annotation security

--- gafico-user/src/main/java/sn/smart/eco/auth/service/impl/StatelessAuthenticationFilter.java
+++ gafico-user/src/main/java/sn/smart/eco/auth/service/impl/StatelessAuthenticationFilter.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
--- war/src/main/java/sn/smart/eco/war/config/SecurityConfig.java
+++ war/src/main/java/sn/smart/eco/war/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,45 +19,44 @@ import sn.smart.eco.auth.service.impl.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+@EnableGlobalMethodSecurity(prePostEnabled=true,securedEnabled=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
-	private  UserDetailsServiceImpl userService;
-	
-	@Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
-	@Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
-	@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
-    }
-	
-	@Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-	
-	@Bean
-    public StatelessAuthenticationFilter statelessAuthenticationFilter() {
-        return new StatelessAuthenticationFilter();
-    }
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().
-                authorizeRequests()
-                .antMatchers("/api/auth/*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http
-                .addFilterBefore(statelessAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+	private UserDetailsServiceImpl userService;
 
+	@Autowired
+	private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public StatelessAuthenticationFilter statelessAuthenticationFilter() {
+		return new StatelessAuthenticationFilter();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+		http.exceptionHandling().and().anonymous().and().servletApi().and().headers().cacheControl();
+		http.authorizeRequests().antMatchers("/api/auth/**").anonymous().antMatchers("/rest/**").authenticated()
+				.anyRequest().permitAll()
+				.and().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(statelessAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 
 }
