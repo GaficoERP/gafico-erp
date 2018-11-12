c4bda8deadc53e561978bbda2f5cb782c6747a9d
Moustapha Mbengue <m2consulting.eu@gmail.com>
2018-11-12 17:17:46 +0000

    fixe(compilation)

--- war/src/test/java/sn/smart/eco/war/common/StructurationRestServiceTest.java
+++ war/src/test/java/sn/smart/eco/war/common/StructurationRestServiceTest.java
@@ -6,12 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import sn.smart.eco.common.model.PlanType;
-import sn.smart.eco.commonjpa.model.LevelType;
-import sn.smart.eco.commonjpa.model.Structuration;
-import sn.smart.eco.war.AbstractRestTest;
-import sn.smart.eco.war.GaficoRestConfigTest;
-
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -25,70 +19,47 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import sn.smart.eco.commonjpa.model.Structuration;
+import sn.smart.eco.war.AbstractRestTest;
+import sn.smart.eco.war.GaficoRestConfigTest;
 
-@ContextConfiguration(classes = {GaficoRestConfigTest.class})
+@ContextConfiguration(classes = { GaficoRestConfigTest.class })
 @WebAppConfiguration
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StructurationRestServiceTest extends AbstractRestTest {
-  private MockMvc mockMvc;
+	private MockMvc mockMvc;
 
-  @Autowired
-  private WebApplicationContext wac;
+	@Autowired
+	private WebApplicationContext wac;
 
-  @Before
-  public void setUp() {
-    MockitoAnnotations.initMocks(this);
-    mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-  }
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
 
-  @Test
-  public void addStructurationTest() throws Exception {
-    Structuration struct = new Structuration("Structuration",
-        new HashSet<>(Arrays.asList(new LevelType("Level0", 0, PlanType.ACCOUNTANCY),
-            new LevelType("Level1", 1, PlanType.ACCOUNTANCY))),
-        PlanType.ACCOUNTANCY);
-    mockMvc
-        .perform(post("/rest/common/struct/add").contentType(MediaType.APPLICATION_JSON)
-            .content(struct.toString()))
-        .andExpect(status().isOk())//
-        .andExpect(jsonPath("name").value("Structuration"));
-  }
+	@Test
+	public void addStructurationTest() throws Exception {
+		Structuration struct = new Structuration("Structuration", null);
+		mockMvc.perform(
+				post("/rest/common/struct/add").contentType(MediaType.APPLICATION_JSON).content(struct.toString()))
+				.andExpect(status().isOk())//
+				.andExpect(jsonPath("name").value("Structuration"));
+	}
 
-  @Test
-  public void findStructurationTest() throws Exception {
-    // has result
-    String name = "Structuration";
-    mockMvc.perform(get("/rest/common/struct/find/{name}", name).accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())//
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
-        .andExpect(jsonPath("name").value(name));
+	@Test
+	public void findStructurationTest() throws Exception {
+		// has result
+		String name = "Structuration";
+		mockMvc.perform(get("/rest/common/struct/find/{name}", name).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())//
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
+				.andExpect(jsonPath("name").value(name));
 
-    // Empty result
-    mockMvc
-        .perform(get("/rest/common/struct/find/{name}", "Name").accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())//
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
-        .andExpect(content().string(""));
-  }
-
-  @Test
-  public void findStructurationByTypeTest() throws Exception {
-    // has result
-    mockMvc
-        .perform(get("/rest/common/struct/findByType/{plan}", PlanType.ACCOUNTANCY)
-            .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())//
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
-        .andExpect(jsonPath("name").value("Structuration"));
-
-    // Empty result
-    mockMvc
-        .perform(get("/rest/common/struct/findByType/{plan}", PlanType.BUDGET)
-            .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())//
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
-        .andExpect(content().string(""));
-  }
+		// Empty result
+		mockMvc.perform(get("/rest/common/struct/find/{name}", "Name").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())//
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))//
+				.andExpect(content().string(""));
+	}
 }
