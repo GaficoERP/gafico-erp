05c18a60e6ce925eb7d27dc89b770ce4be066cde
ASyll1 <44465554+ASyll1@users.noreply.github.com>
2018-12-20 18:31:55 +0000

    restaurer app-module

---
+++ erp-viewer/src/app/app.module.ts
@@ -0,0 +1,48 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from "./shared/shared.module";
+import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from "./layouts/content/content-layout.component";
+import { FullLayoutComponent } from "./layouts/full/full-layout.component";
+import { CustomOption } from "./shared/toastr/custom-option";
+import { FormsModule } from '@angular/forms';
+import { JwtInterceptor } from './helpers/jwt.interceptor';
+import * as $ from 'jquery';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './services/authentication.service';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        FullLayoutComponent,
+        ContentLayoutComponent
+  
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule,
+        HttpClientModule,
+        ToastModule.forRoot(),
+        NgbModule.forRoot(),
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyBr5_picK8YJK7fFR2CPzTVMj6GG1TtRGo'
+        }),
+        FormsModule
+    ],
+    providers: [AuthenticationService,
+        { provide: ToastOptions, useClass: CustomOption },{
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true
+          }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
